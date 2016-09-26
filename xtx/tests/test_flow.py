2a978c8a87a895711b8e3df9ca7fd6293f3ad86e
lipei86 <pei@ilanever.com>
2016-09-26 16:20:57 +0800

    refact test files.

--- xtx/tests/test_flow.py
+++ xtx/tests/test_flow.py
@@ -12,11 +12,20 @@ class SimpleFlowTest(unittest.TestCase):
 		pass
 		
 	def test_from_excel_to_postgres(self):
-		file_path = ""
+		file_path = "data/test.xlsx"
+		sheetIndex = 0
 		db_url = ""
 		db_username = ""
 		db_userpwd = ""
-		
+		rules = [
+			Rule(column = "日期", type = Types.Date, targetField = "date", emptyHandler, unmatchHandler)
+			, Rule(column = "类型", type = Types.Enum, targetField = "type", emptyHandler, unmatchHandler, saveRawField = "type_raw")
+			, Rule(column = "银行", type = Types.String, targetField = "bank", emptyHandler, unmatchHandler, saveRawField = "bank_raw")
+			, Rule(column = "卡类型", type = Types.Enum, targetField = "cardtype", emptyHandler, unmatchHandler, saveRawField = "cardtype_raw")
+			, Rule(column = "用户数", type = Types.Integer, targetField = "usersCount", emptyHandler, unmatchHandler)
+			, Rule(column = "卡数", type = Types.Integer, targetField = "cardsCount", emptyHandler, unmatchHandler)
+		]
+		loader = Loader(data = dat ,rules = rules)
 		# self.assertEqual(1, 2)
 		
 
