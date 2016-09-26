1cf475d83e9e11586220b29aceae15f5558d32c2
lipei86 <pei@ilanever.com>
2016-09-26 10:35:45 +0800

    create flow test.

--- test/flow_test/dk_flow.py
+++ test/flow_test/dk_flow.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8  -*-
 
-def read_eml():
-	pass
+import unittest
+
+class SimpleFlowTest(unittest.TestCase):
+	def setUp(self):
+		pass
+		
+	def tearDown(self):
+		pass
+		
+	def from_eml_to_excel(self):
+		self.assertEqual(1, 2)
+		
+
+def suite():
+	suite = unittest.TestSuite()
+	suite.addTest(SimpleFlowTest("from_eml_to_excel"))
+	return suite
 	
-def 
+if __name__ == "__main__":
+	unittest.main(defaultTest = "suite")
