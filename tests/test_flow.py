7803ab70cff2461bfa64b95e2d5c7e05cdccc7a3
lipei86 <pei@ilanever.com>
2016-10-08 14:09:15 +0800

    add vs project file.

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -92,24 +92,24 @@ class SimpleFlowTest(unittest.TestCase):
 		df_src = excelExtractor.extract(filepath = file_path, sheetIndex = 0)
 
 		# transform
-		card_fact =
+		#card_fact =
 
 		# load
-		db_url = ""
-		db_username = ""
-		db_userpwd = ""
-		loadRules = [
-			Rule(column = "日期", type = Types.Date, targetField = "date", emptyHandler, unmatchHandler)
-			, Rule(column = "类型", type = Types.Enum, targetField = "type", emptyHandler, unmatchHandler, saveRawField = "type_raw")
-			, Rule(column = "银行", type = Types.String, targetField = "bank", emptyHandler, unmatchHandler, saveRawField = "bank_raw")
-			, Rule(column = "卡类型", type = Types.Enum, targetField = "cardtype", emptyHandler, unmatchHandler, saveRawField = "cardtype_raw")
-			, Rule(column = "用户数", type = Types.Integer, targetField = "usersCount", emptyHandler, unmatchHandler)
-			, Rule(column = "卡数", type = Types.Integer, targetField = "cardsCount", emptyHandler, unmatchHandler)
-		]
-		tableLoadRule = TableLoadRule(targetSchemaName = "jd_sys", targetTableName="users", autoCreateSchema = True, appendType = AppendType.AppendAfterCleanCascade)
-		tableLoadRule.addColumnRule(ColumnRule(sourceColumn = "日期", datatype = DataTypes.Date, targetField = "date", emptyHandler = ignoreEmptyHandler, unmatchHandler = alertUnmatchHander))
+		#db_url = ""
+		#db_username = ""
+		#db_userpwd = ""
+		#loadRules = [
+		#	Rule(column = "日期", type = Types.Date, targetField = "date", emptyHandler, unmatchHandler)
+		#	, Rule(column = "类型", type = Types.Enum, targetField = "type", emptyHandler, unmatchHandler, saveRawField = "type_raw")
+		#	, Rule(column = "银行", type = Types.String, targetField = "bank", emptyHandler, unmatchHandler, saveRawField = "bank_raw")
+		#	, Rule(column = "卡类型", type = Types.Enum, targetField = "cardtype", emptyHandler, unmatchHandler, saveRawField = "cardtype_raw")
+		#	, Rule(column = "用户数", type = Types.Integer, targetField = "usersCount", emptyHandler, unmatchHandler)
+		#	, Rule(column = "卡数", type = Types.Integer, targetField = "cardsCount", emptyHandler, unmatchHandler)
+		#]
+		#tableLoadRule = TableLoadRule(targetSchemaName = "jd_sys", targetTableName="users", autoCreateSchema = True, appendType = AppendType.AppendAfterCleanCascade)
+		#tableLoadRule.addColumnRule(ColumnRule(sourceColumn = "日期", datatype = DataTypes.Date, targetField = "date", emptyHandler = ignoreEmptyHandler, unmatchHandler = alertUnmatchHander))
 
-		loader = Loader(data = dat ,rules = loadRules, recreateSchema = True)
+		#loader = Loader(data = dat ,rules = loadRules, recreateSchema = True)
 		# self.assertEqual(1, 2)
 
 
