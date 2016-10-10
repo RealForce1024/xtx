18c5d5c5384286a15196153ffb82fc6bcfff766f
lipei86 <pei@ilanever.com>
2016-10-10 13:37:48 +0800

    refact excel storage and tests.

--- xtx/storage/__init__.py
+++ xtx/storage/__init__.py
@@ -6,7 +6,9 @@ from .file_storage import FileStorage
 from .text_file_storage import TextFileStorage
 from .bin_file_storage import BinFileStorage
 from .csv_storage import CsvStorage
-from .excel_storage import (Excel2003Storage, Excel2007Storage, ExcelStorage)
+from .excel_2003_storage import Excel2003Storage
+from .excel_2007_storage import Excel2007Storage
+from .excel_storage import ExcelStorage
 from .eml_storage import EmlStorage
 from .html_storage import HtmlStorage
 from .dbtable_storage import DbTableStorage
